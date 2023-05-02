322417f4ab5f054c8894cca49f49c31f035a05cb
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-05-02 15:08:33 -0600

    edit and delete categories

--- ecommerce_admin/pages/categories.js
+++ ecommerce_admin/pages/categories.js
@@ -1,27 +1,74 @@
 import Layout from '@/components/Layout';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { withSwal } from 'react-sweetalert2';
 
-export default function Categories() {
+function Categories({ swal }) {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState('');
+  const [parentCategory, setParentCategory] = useState('');
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
     axios.get('/api/categories').then((result) => {
       setCategories(result.data);
     });
-  }, []);
+  }
 
   async function saveCategory(e) {
     e.preventDefault();
-    await axios.post('/api/categories', { name });
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put('/api/categories', data);
+      setEditedCategory(null);
+    } else {
+      await axios.post('/api/categories', data);
+    }
     setName('');
+    fetchCategories();
+  }
+
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+
+  function deleteCategory(category) {
+    swal
+      .fire({
+        title: 'Are you sure?',
+        text: `Do you want to delete ${category.name}?`,
+        showCancelButton: true,
+        cancelButtonText: 'Cancel',
+        confirmButtonText: 'Yes, Delete!',
+        confirmButtonColor: '#d55',
+        reverseButtons: true,
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          await axios.delete('/api/categories?_id=' + category._id);
+          fetchCategories();
+        }
+      })
+      .catch((error) => {
+        // when promise rejected...
+      });
   }
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label> New category name</label>
+      <label>
+        {editedCategory
+          ? `Edit Category ${editedCategory.name}`
+          : 'Create New Category'}
+      </label>
       <form
         onSubmit={saveCategory}
         className='flex gap-1'>
@@ -32,6 +79,16 @@ export default function Categories() {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <select
+          className='mb-0'
+          value={parentCategory}
+          onChange={(e) => setParentCategory(e.target.value)}>
+          <option value=''>No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <button
           type='submit'
           className='btn-primary p-1'>
@@ -42,6 +99,8 @@ export default function Categories() {
         <thead>
           <tr>
             <td>Category Name</td>
+            <td>Parent Category</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
@@ -49,6 +108,19 @@ export default function Categories() {
             categories.map((category) => (
               <tr>
                 <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    className='btn-primary mr-1'
+                    onClick={() => editCategory(category)}>
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className='btn-primary'>
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
@@ -56,3 +128,5 @@ export default function Categories() {
     </Layout>
   );
 }
+
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
